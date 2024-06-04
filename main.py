e33b7946da7900dacffc94921e3b3feea2735d7a
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-06-04 18:53:29 +0530

    Generated Count,Total or Average of car sales based on user input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#To display highest car sales volume by each company
+#To generate Count,Total or Average of car sales based on user input.
+import sys
 import pandas as pd
 from tabulate import tabulate
 
@@ -6,21 +7,34 @@ def read_input(csv_path):
     df = pd.read_csv(csv_path)
     return df
 
-def process_input(car_sales_data):
-    car_sales_data= car_sales_data.groupby("Company")["Price($)"].sum()
-    # Reset index and rename columns to avoid Companies as index labels
-    car_sales_data = car_sales_data.reset_index()
-    car_sales_data = car_sales_data.rename(columns = {'Price($)': 'TOTAL_REVENUE($)'})
-    return car_sales_data
+def calc_report(user_query):
+    generated_report = user_query["car_sales"].agg(user_query["report"]).reset_index()
+    generated_report = generated_report.rename(columns={"Price($)": user_query['col_name']})
+    return generated_report
+
+def process_input(user_query):
+    user_query["car_sales"] =  user_query["data"].groupby(user_query["field_name"])["Price($)"]
+    output = calc_report(user_query)
+    return output
 
 def display_output(customer_purchases):
     print(tabulate(customer_purchases.values.tolist(),
     headers=list(customer_purchases),tablefmt="grid"))
-    
+
 def main():
+    #User Input:Field Name ReportToGenerate NewColumnName 
+    # sample Company sum/mean/count coln_name
+    field_name = sys.argv[1]
+    report_to_generate = sys.argv[2]
+    col_rename = sys.argv[3]
     csv_path = "car_sales.csv"
     car_sales_data = read_input(csv_path)
-    company_sales = process_input(car_sales_data)
+    user_query = {
+    "data":car_sales_data,
+    "field_name":field_name,
+    "report": report_to_generate,
+    "col_name": col_rename}
+    company_sales = process_input(user_query)
     display_output(company_sales)
 
 if __name__ == "__main__":
